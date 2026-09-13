fda8d1ee1e079dcb13ed413bbaeacda8f7350f51
Maneetbal <128986205+Maneetbal@users.noreply.github.com>
2026-09-12 21:59:02 -0600

    Replace Purpur/MultiPaper build wiring with Folia 26.2 upstream layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,53 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import io.papermc.paperweight.tasks.RebuildGitPatches
 
 plugins {
     java // TODO java launcher tasks
     id("io.papermc.paperweight.patcher") version "2.0.0-beta.21"
 }
 
-val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
-
 paperweight {
-    upstreams.register("purpur") {
-        repo = github("PurpurMC", "Purpur")
-        ref = providers.gradleProperty("purpurRef")
+    filterPatches = false
+    upstreams.paper {
+        ref = providers.gradleProperty("paperRef")
 
         patchFile {
-            path = "purpur-server/build.gradle.kts"
-            outputFile = file("multipaper-server/build.gradle.kts")
-            patchFile = file("multipaper-server/build.gradle.kts.patch")
+            path = "paper-server/build.gradle.kts"
+            outputFile = file("folia-server/build.gradle.kts")
+            patchFile = file("folia-server/build.gradle.kts.patch")
         }
         patchFile {
-            path = "purpur-api/build.gradle.kts"
-            outputFile = file("multipaper-api/build.gradle.kts")
-            patchFile = file("multipaper-api/build.gradle.kts.patch")
+            path = "paper-api/build.gradle.kts"
+            outputFile = file("folia-api/build.gradle.kts")
+            patchFile = file("folia-api/build.gradle.kts.patch")
         }
-        patchRepo("paperApi") {
+        patchFile {
+            path = "paper-checkstyle/build.gradle.kts"
+            outputFile = file("folia-checkstyle/build.gradle.kts")
+            patchFile = file("folia-checkstyle/build.gradle.kts.patch")
+        }
+        patchDir("paperApi") {
             upstreamPath = "paper-api"
-            patchesDir = file("multipaper-api/paper-patches")
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("folia-api/paper-patches")
             outputDir = file("paper-api")
         }
-        patchDir("purpurApi") {
-            upstreamPath = "purpur-api"
-            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
-            patchesDir = file("multipaper-api/purpur-patches")
-            outputDir = file("purpur-api")
+        patchDir("paperCheckstyle") {
+            upstreamPath = "paper-checkstyle"
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("folia-checkstyle/paper-patches")
+            outputDir = file("paper-checkstyle")
+        }
+        patchDir("paperCheckstyleConfig") {
+            upstreamPath = ".checkstyle"
+            patchesDir = file("folia-checkstyle/config-patches")
+            outputDir = file(".checkstyle")
         }
     }
 }
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 subprojects {
     apply(plugin = "java-library")
@@ -50,6 +62,10 @@ subprojects {
     repositories {
         mavenCentral()
         maven(paperMavenPublicUrl)
+    }
+
+    dependencies {
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
     }
 
     tasks.withType<AbstractArchiveTask>().configureEach {
@@ -73,5 +89,26 @@ subprojects {
             exceptionFormat = TestExceptionFormat.FULL
             events(TestLogEvent.STANDARD_OUT)
         }
+    }
+
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven("https://artifactory.papermc.io/artifactory/releases/") {
+                name = "paperReleases"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(project.version)
     }
 }
